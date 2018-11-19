d43dd3f8c175d2343b8e69efa24800ea63150005
seoilgun <seoilgun@gmail.com>
2018-11-20 00:37:06 +0900

    test

--- trainer.py
+++ trainer.py
@@ -133,10 +133,12 @@ def main(cf):
         saver.save(sess, cf.save_dir + "/model.ckpt", epoch)
     if cf.eval_after_training:
         os.system(
-            "python multiple_search_models.py --model_dir=%s --embedding_size=%d --data_dir=%s --model_name=%s --max_top_k=%d" %
-            (cf.save_dir, cf.embedding_size, cf.data_dir, cf.model_name, cf.eval_max_top_k))
-    if cf.shutdown_after_train:
-        os.system("sudo shutdown now")
+            "nohup python -u multiple_search_models.py --model_dir=%s --embedding_size=%d --data_dir=%s --model_name=%s --max_top_k=%d --shutdown_after_train=%d > %s/search.log &" %
+            (cf.save_dir, cf.embedding_size, cf.data_dir, cf.model_name, cf.eval_max_top_k,
+             1 if cf.shutdown_after_train else 0, cf.save_dir))
+    else:
+        if cf.shutdown_after_train:
+            os.system("sudo shutdown now")
 
 
 def train():
