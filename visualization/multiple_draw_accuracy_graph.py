d0294e8e3ab10b7a42fffebb42f86d83f9bd3ea8
seoilgun <seoilgun@gmail.com>
2018-11-19 14:27:09 +0900

    test

---
+++ visualization/multiple_draw_accuracy_graph.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import glob, os
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_dir', default='experiments/alexnet',
+                    help="Experiment directory containing params.json")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    matplotlib.use('Agg')
+    result_path = os.path.join(args.model_dir, "search_result")
+    result_files = glob.glob(os.path.join(result_path, "*.json"))
+    legends = []
+    for result_file in result_files:
+        i = int(result_file.split("_")[0])
+        accuracies = json.load(open(result_file))
+        plt.plot(accuracies)
+        legends.append("epoch %d" % i)
+    plt.legend(legends, loc='upper left')
+    plt.savefig("accuracy_graph.png")
