862c5c2868847c276c7d7ea8c7716d323b865dc9
seoilgun <seoilgun@gmail.com>
2018-11-19 13:23:28 +0900

    test

--- multiple_search_models.py
+++ multiple_search_models.py
@@ -10,4 +10,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     model_epochs = [os.path.basename(path).split("-")[1].split('.')[0] for path in
                     glob.glob(os.path.join(args.model_dir, "model*.index"))]
+    model_epochs.sort()
     json.dump(model_epochs, open(os.path.join(args.model_dir, "search_epochs.json"), mode="w+"))
