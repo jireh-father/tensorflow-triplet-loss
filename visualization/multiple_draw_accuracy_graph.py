c04b7ab701c8f77ee103809db6f3a06899cb4cfe
seoilgun <seoilgun@gmail.com>
2018-11-19 18:05:06 +0900

    test

--- visualization/multiple_draw_accuracy_graph.py
+++ visualization/multiple_draw_accuracy_graph.py
@@ -1,36 +1,35 @@
 import matplotlib.pyplot as plt
-import matplotlib
 import glob, os
 import argparse
 import json
+import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_dir', default='experiments/alexnet',
+parser.add_argument('--model_dir', default='d:/result/triplet',
                     help="Experiment directory containing params.json")
 parser.add_argument('--epoch_list', default=None,
                     help="Directory containing the dataset")
-
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    matplotlib.use('Agg')
     result_path = os.path.join(args.model_dir, "search_result")
     result_files = glob.glob(os.path.join(result_path, "*_accuracies.json"))
+    assert len(result_files) > 0
     legends = []
-    if args.epoch_list is None:
-        for result_file in result_files:
-            i = int(os.path.basename(result_file).split("_")[0])
-            accuracies = json.load(open(result_file))
-            plt.plot(accuracies)
-            legends.append("epoch %d" % i)
-    else:
+    fig, ax = plt.subplots(figsize=(12, 12))
+    accuracies = json.load(open(result_files[0]))
+    index = np.arange(len(accuracies))
+    bar_width = 0.15
+    model_epochs = None
+    if args.epoch_list is not None:
         model_epochs = [int(e) for e in args.epoch_list.split(",")]
-        for result_file in result_files:
-            i = int(os.path.basename(result_file).split("_")[0])
-            if i not in model_epochs:
-                continue
-            accuracies = json.load(open(result_file))
-            plt.plot(accuracies)
-            legends.append("epoch %d" % i)
+    for epoch, result_file in enumerate(result_files):
+        i = int(os.path.basename(result_file).split("_")[0])
+        if args.epoch_list is not None and i not in model_epochs:
+            continue
+        accuracies = json.load(open(result_file))
+        ax.bar(index+(epoch * bar_width), accuracies, bar_width, label="epoch_%d" % i)
+        # plt.plot(accuracies)
+        legends.append("epoch %d" % i)
     plt.legend(legends, loc='upper left')
     plt.savefig("accuracy_graph.png")
