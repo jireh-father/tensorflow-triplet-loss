bf4492db8db35fa656a9c12933e59d29cae8dc56
seoilgun <seoilgun@gmail.com>
2018-12-04 00:20:23 +0900

    test

--- web_app_cls.py
+++ web_app_cls.py
@@ -3,6 +3,9 @@ import tensorflow as tf
 from preprocessing import preprocessing_factory
 from model import nets_factory
 import os, uuid
+from grad_cam_plus_plus import GradCamPlusPlus
+import cv2
+from PIL import Image
 
 preprocessing_name = 'inception'
 model_name = 'inception_resnet_v2'
@@ -28,6 +31,7 @@ label_map = {0: "conservative",
              13: "street"}
 
 UPLOAD_DIR = 'static/upload'
+CAM_DIR = 'static/cam'
 ALLOWED_EXTENSIONS = ['jpg']
 
 app = Flask(__name__)
@@ -54,8 +58,11 @@ iterator = dataset.make_initializable_iterator()
 images_op = iterator.get_next()
 
 model_f = nets_factory.get_network_fn(model_name, num_classes, is_training=False)
-logits, _ = model_f(images_op)
+logits, end_points = model_f(images_op)
 result_prob_op = tf.nn.softmax(logits)
+
+cam = GradCamPlusPlus(logits, end_points["Conv2d_7b_1x1"], images_op)
+
 saver = tf.train.Saver(tf.global_variables())
 
 tf_config = tf.ConfigProto()
@@ -96,11 +103,23 @@ def upload():
 def classification():
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], request.args['file_name'])
     sess.run(iterator.initializer, feed_dict={file_names: [file_path]})
-    result_probs = sess.run(result_prob_op)
+    result_probs, images_input, logits_input = sess.run([result_prob_op, images_op, logits])
     result_idx = result_probs[0].argmax()
 
+    cam_imgs, class_indices = cam.create_cam_imgs(sess, images_input, logits_input)
+    heatmap = cam.convert_cam_2_heatmap(cam_imgs[0][0])
+    overlay_img = cam.overlay_heatmap(images_input[0], heatmap)
+    cam_image = overlay_img[..., ::-1]
+    print(cam_image.shape)
+    cam_filename = "%s.jpg" % uuid.uuid4()
+    if not os.path.isdir("static/cam"):
+        os.makedirs("static/cam")
+    Image.fromarray(cam_image).save(os.path.join("static/cam", cam_filename))
+    print("finished to summary cam")
+
     return render_template("classification.html", query_file_name=request.args['file_name'], result_idx=result_idx,
-                           result_probs=result_probs[0], label_map=label_map, num_classes=num_classes)
+                           result_probs=result_probs[0], label_map=label_map, num_classes=num_classes,
+                           cam_filename=cam_filename)
 
 
 def allowed_file(file_name):
