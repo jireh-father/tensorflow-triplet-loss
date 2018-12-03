2d0011cee464ac4e868ce4269fae04b88765c2e1
seoilgun <seoilgun@gmail.com>
2018-12-03 14:33:16 +0900

    test

--- web_app.py
+++ web_app.py
@@ -134,10 +134,11 @@ def search():
         result_file_name = os.path.join(SEARCHED_DIR, sub_dir, "%d.jpg" % i)
         if not os.path.isdir(os.path.dirname(result_file_name)):
             os.makedirs(os.path.dirname(result_file_name))
-        result_file_names.append(result_file_name)
+        result_file_names.append(os.path.basename(result_file_name))
         result_image.save(result_file_name)
     return render_template("search.html", query_file_name=request.args['file_name'], result_sub_dir=sub_dir,
-                           result_file_names=result_file_names, num_result_images=max_top_k, result_dist_list=searched_dist_list[0])
+                           result_file_names=result_file_names, num_result_images=max_top_k,
+                           result_dist_list=searched_dist_list[0])
 
 
 def allowed_file(file_name):
