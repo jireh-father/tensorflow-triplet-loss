c8a3b3efbf4c68f63d58ee3bedaa8e42451f6a80
seoilgun <seoilgun@gmail.com>
2018-12-19 11:16:17 +0900

    test

--- preprocessing/inception_preprocessing.py
+++ preprocessing/inception_preprocessing.py
@@ -274,8 +274,9 @@ def preprocess_for_eval(image, height, width,
     if height and width:
       # Resize the image to the specified height and width.
       image = tf.expand_dims(image, 0)
-      image = tf.image.resize_bilinear(image, [height, width],
-                                       align_corners=False)
+      # image = tf.image.resize_bilinear(image, [height, width],
+      #                                  align_corners=False)
+      image = tf.image.resize_image_with_pad(image, height, width)
       image = tf.squeeze(image, [0])
     image = tf.subtract(image, 0.5)
     image = tf.multiply(image, 2.0)
